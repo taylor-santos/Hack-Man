ad876124a85311c6295d97ac3bc065bb6051b24a
taylorules <taylor.p.santos@gmail.com>
2017-02-21 20:52:41 -0800

    V1.3
    Implemented bug direction. It was discovered that bugs cannot reverse
    their direction, which significantly reduces the number of choices that
    they can make. I have implemented 4 more lookup tables for bug movement,
    each one corresponding to a different direction. Each table shows the
    optimal path to take assuming that the bug cannot move backwards in the
    direction it came from. As a result, the bot will be a lot less cautious
    around bugs, because it only predicts legal bug movement. This will make
    navigation far easier, especially when there are several bugs on the
    board.

--- Hack-Man/Grid.h
+++ Hack-Man/Grid.h
@@ -16,11 +16,11 @@ public:
 	vector<Cell*> snippets;
 	vector<Cell*> weapons;
 	vector<Cell*> bugs;
+	vector<int> bug_directions;
 
 	int height() { return h; }
 	int width() { return w; }
 	Grid* copy();
-	void make_bugpath();
 	void reset();
 	Grid(int, int);
 	~Grid();
